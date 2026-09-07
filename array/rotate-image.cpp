22d1f1154ca54ea537554c160ad676743a42c7ac
DharanK29 <dharank291207@gmail.com>
2026-09-07 19:22:59 +0530

    LC-rotate-image [Runtime: N/A; Memory: N/A]

---
+++ array/rotate-image.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        int front = 0;
+        int end = n-1;
+
+        while(front<end){
+            for(int i=0;i<n;i++){
+                swap(matrix[i][front],matrix[i][end]);
+            }
+            front++;
+            end--;
+        }
+    }
+};
+
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
