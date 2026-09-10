4238454346d439fb2e5aec03c9a24e57203f91d7
DharanK29 <dharank291207@gmail.com>
2026-09-10 20:28:36 +0530

    LC-convert-sorted-array-to-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ array/convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BuildTree(nums,0,nums.size()-1);
+    }
+
+    TreeNode* BuildTree(vector<int>& nums,int left,int right){
+        if(left > right) return nullptr;
+
+        int mid = left+(right-left)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = BuildTree(nums,left,mid-1);
+        root->right = BuildTree(nums,mid+1,right);
+
+        return root;
+    }
+};
+
+#pragma GCC optimize("Ofast")
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
